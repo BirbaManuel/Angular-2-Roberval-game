7cb4c6e92a468231a2080860a036f1342d6d9eef
Manuel Birba <manuelbirba@hotmail.fr>
2019-05-14 10:02:31 +0200

    finale

--- my-app/src/app/list-balls/list-balls.component.ts
+++ my-app/src/app/list-balls/list-balls.component.ts
@@ -8,10 +8,6 @@ import {
   Validators,
   FormControl,
 } from '@angular/forms'
-/*
-import { from } from 'rxjs'
-import { map } from 'rxjs/operators/'
-*/
 
 import { BallsGeneratorService } from './../services/balls-generator.service'
 import { HeavyweightBallComponent } from './../heavyweight-ball/heavyweight-ball.component'
@@ -29,8 +25,8 @@ export class ListBallsComponent implements OnInit {
   showResultat: Boolean
   resultatBouleMax: Number
   balls$: any
-  // balls$: Ball[]
   ballss: Observable<any>
+  gamedata: any
   constructor(
     private fb: FormBuilder,
     private ballsGenerator: BallsGeneratorService
@@ -42,34 +38,27 @@ export class ListBallsComponent implements OnInit {
 
   reset() {
     this.showResultat = false
+    this.ballForm.reset()
   }
 
-  findBouleMax(arrayOfNumber) {
-    console.log(`La boule ${maxArrayNumber(arrayOfNumber)} est la plus lourde`)
-    this.showResultat = true
-    this.resultatBouleMax = maxArrayNumber(arrayOfNumber)
-    this.resultatIteration = 1
-  }
   ngOnInit() {
-    // const ballUnitSchema = this.fb.group({
-    //   poids1: '',
-    //   poids2: '',
-    //   poids3: '',
-    //   poids4: '',
-    // })
-    this.setBallsControl = this.fb.control(
-      'setBalls',
-      Validators.pattern('^(?:[1-9]|[1-4][0-9]|50)$') // nombre de 0 à 50
-    )
-    this.ballForm = this.fb.group({
-      titre: 'Robervall-game',
-      setBalls: this.setBallsControl,
-      phones: this.fb.array([]),
-    })
+    //this.gamedata = gameBallsPromise()
+    // console.log(this.gamedata)
     this.ballsGenerator.getAllBalls().subscribe(
       sucess => {
         this.balls$ = sucess['data']
-        console.log(sucess)
+        console.log(this.balls$)
+        this.ballForm = this.fb.group({
+          titre: 'Robervall-game',
+          boule0: this.balls$[0]['poids'],
+          boule1: this.balls$[1]['poids'],
+          boule2: this.balls$[2]['poids'],
+          boule3: this.balls$[3]['poids'],
+          boule4: this.balls$[4]['poids'],
+          boule5: this.balls$[5]['poids'],
+          boule6: this.balls$[6]['poids'],
+          boule7: this.balls$[7]['poids'],
+        })
       },
       error => {
         this.balls$ = BALLS
@@ -77,40 +66,26 @@ export class ListBallsComponent implements OnInit {
       }
     )
     this.showResultat = false
-    this.ballForm.valueChanges.subscribe(sucess => {
-      this.log(sucess)
-    })
-  }
-  get phoneForms() {
-    return this.ballForm.get('phones') as FormArray
-  }
-  get setBallsForms() {
-    return this.ballForm.get('setBalls')
-  }
-  addPhone() {
-    const phone = this.fb.group({
-      poids: [],
-    })
-    this.phoneForms.push(phone)
-  }
-  deletePhone(i) {
-    this.phoneForms.removeAt(i)
   }
 
   async submitHandler() {
-    const formValue = this.ballForm.value
+    const { titre, ...formValue } = this.ballForm.value
+    const values = Object.values(formValue)
     try {
-      await console.log(formValue)
-      // await console.log(formValue)
+      const result = maxArrayStrictNumber(values)
+      console.log(result)
+      this.resultatBouleMax = result
     } catch (err) {}
+    this.showResultat = true
   }
 }
-function maxArrayNumber(arrayOfNumber: Ball[]) {
+
+function maxArrayStrictNumber(arrayOfNumber) {
   //only one iteration
   return Math.max(
     ...arrayOfNumber.map(el => {
-      return el.poids
+      return el
     })
   )
 }
-export const maxArrayNumberFCT = maxArrayNumber
+export const maxArrayNumberFCT = maxArrayStrictNumber
